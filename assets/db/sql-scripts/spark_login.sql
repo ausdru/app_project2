b855abdc7fe7c1af734b9d7cd4110b0ecf867021
Steven Gray <stevegray@granite-mtn.com>
2023-11-28 17:11:44 -0700

    Added from_date to login table primary key. dropped person_role table.

--- assets/db/sql-scripts/spark_login.sql
+++ assets/db/sql-scripts/spark_login.sql
@@ -20,7 +20,6 @@
 --
 
 DROP TABLE IF EXISTS `login`;
-
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `login` (
@@ -55,4 +54,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-11-28 15:37:40
+-- Dump completed on 2023-11-28 17:08:11
